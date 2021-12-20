86581ea4f64b1b28e1d31f67cd9826883fb04a01
ctessierpiart <c.tessierpiart@gmail.com>
2021-12-20 13:32:53 +0100

    Solved Day15

--- Day15/Riddle15.py
+++ Day15/Riddle15.py
@@ -55,47 +55,50 @@ def update_line(Distance_to_source, Predecessor, step):
         
     return Updated
       
-# Cave = np.array([[1,9,9,1,1,1,1,1,1,1],
-# [1,9,9,1,9,9,9,9,9,1],
-# [1,9,9,1,1,9,9,9,9,1],
-# [1,9,9,9,1,9,9,9,9,1],
-# [1,9,9,9,1,9,9,9,9,1],
-# [1,1,1,1,1,9,9,9,9,1],
-# [9,9,9,9,9,9,9,9,9,1],
-# [9,9,9,9,9,9,9,9,9,1],
-# [9,9,9,9,9,9,9,9,9,1]])
+# Cave = np.array([[1,1,6,3,7,5,1,7,4,2],
+# [1,3,8,1,3,7,3,6,7,2],
+# [2,1,3,6,5,1,1,3,2,8],
+# [3,6,9,4,9,3,1,5,6,9],
+# [7,4,6,3,4,1,7,1,1,1],
+# [1,3,1,9,1,2,8,1,3,7],
+# [1,3,5,9,9,1,2,4,2,1],
+# [3,1,2,5,4,2,1,6,3,9],
+# [1,2,9,3,1,3,8,5,2,1],
+# [2,3,1,1,9,4,4,5,8,1]])
     
-# Size_y, Size_x = Cave.shape    
-# Shape_predecessor = (Size_y, Size_x, 2)
-# max_distance = Size_x + Size_y
+# Part 1
 
-# Distance_to_source = np.zeros(Cave.shape).astype(np.int32)
-# Distance_to_source.fill(np.iinfo(np.int32).max)
-# Predecessor = np.zeros(Shape_predecessor).astype(np.int32)
-# Predecessor.fill(0)
+Size_y, Size_x = Cave.shape    
+Shape_predecessor = (Size_y, Size_x, 2)
+max_distance = Size_x + Size_y
 
-# Distance_to_source[0,0] = 0
+Distance_to_source = np.zeros(Cave.shape).astype(np.int32)
+Distance_to_source.fill(np.iinfo(np.int32).max)
+Predecessor = np.zeros(Shape_predecessor).astype(np.int32)
+Predecessor.fill(0)
 
-# for step in range(max_distance):
-#     update_line(Distance_to_source, Predecessor, step)
+Distance_to_source[0,0] = 0
+
+for step in range(max_distance):
+    update_line(Distance_to_source, Predecessor, step)
     
-#     update_step = step - 1
-#     while update_step > 0:
-#         is_updated = update_line(Distance_to_source, Predecessor, update_step)
-#         if is_updated == True:
-#             update_step = step
-#         update_step -= 1
+    update_step = step - 1
+    while update_step > 0:
+        is_updated = update_line(Distance_to_source, Predecessor, update_step)
+        if is_updated == True:
+            update_step = step
+        update_step -= 1
         
-# print(Distance_to_source)
+print(Distance_to_source)
 
 # Part 2
 for i in range(5):
-    new_line = Cave + i
-    new_line[new_line > 9] = 1
-    new_tile = new_line
+    new_tile = Cave + i
+    new_tile[new_tile > 9] -= 9
+    new_line = new_tile
     for j in range(1,5):
         new_tile = new_tile + 1
-        new_tile[new_tile > 9] = 1
+        new_tile[new_tile > 9] -= 9
         new_line = np.append(new_line, new_tile, axis = 0)
     if i == 0:
         new_cave = new_line
