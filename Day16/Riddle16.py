cb13734206e610088997206eec1419b33fda5b56
Charles TESSIERPIART <charles.tessierpiart@legrand-ext.com>
2022-04-05 10:49:39 +0200

    Solved day 16

--- Day16/Riddle16.py
+++ Day16/Riddle16.py
@@ -1,22 +1,28 @@
+from math import prod
+
+
 with open('Day16/Packets.csv', 'r') as file:
             line = file.read().strip('\n')
             bit_packets = bin(int(line, 16))[2:]
 
 class Packet():
     def __init__(self, bit_packet):
-        self.subpackets = []
+        self.Leftovers = []
         self.Version = 0
+        self.sumVersion = 0
         self.Type = 0
         ##Literal value
         self.Value = 0
         ##Operator
         self.Length_ID = 0
         self.Length = 0
+        self.subpacketValues = []
         self.Packet_decode(bit_packet)
         
     def Packet_decode(self, bit_packet):
         index = 0
         self.Version = int(bit_packet[index:index+3], 2)
+        self.sumVersion = self.Version
         index += 3
         self.Type = int(bit_packet[index:index+3], 2)
         index += 3
@@ -33,7 +39,8 @@ class Packet():
             stringValue = ''.join([stringValue, bit_packet[index:index+4]])
             index += 4
             self.Value += int(stringValue, 2)
-            
+            self.Length = index
+            self.Leftovers = bit_packet[index:]
         
         else:
             self.Length_ID = (bit_packet[index] == '1')
@@ -41,22 +48,48 @@ class Packet():
             if self.Length_ID == True: ##Number of subpackets
                 TotalSubpacketNumber = int(bit_packet[index:index+11],2)
                 index += 11
+                self.Length = index
                 SubpacketNumber = 0
-                LeftoversBit = bit_packet[index:]
+                self.Leftovers = bit_packet[index:]
                 while SubpacketNumber != TotalSubpacketNumber:
                     SubpacketNumber += 1
-                    LeftoversBit, Packetlenght = Packet(LeftoversBit)
-                    self.Length += Packetlenght
+                    Subpacket = Packet(self.Leftovers)
+                    self.subpacketValues.append(Subpacket.Value)
+                    self.Length    += Subpacket.Length
+                    self.Leftovers  = Subpacket.Leftovers
+                    self.sumVersion += Subpacket.sumVersion
                     
             else:                      ##Bits of subpackets
-                self.Length = int(bit_packet[index:index+15],2)
+                self.Length = index + 15 + int(bit_packet[index:index+15],2)
+                SubpacketLength = int(bit_packet[index:index+15],2)
                 index += 15
                 TotalLength = 0
-                LeftoversBit = bit_packet[index:]
-                while TotalLength != self.Length:
-                    LeftoversBit, Packetlenght = Packet(LeftoversBit)
-                
-        return bit_packet[index:], self.Length
+                self.Leftovers = bit_packet[index:]
+                while TotalLength != SubpacketLength:
+                    Subpacket = Packet(self.Leftovers)
+                    TotalLength    += Subpacket.Length
+                    self.Leftovers  = Subpacket.Leftovers
+                    self.sumVersion += Subpacket.sumVersion
+                    self.subpacketValues.append(Subpacket.Value)
+                    
+            match(self.Type):
+                case(0):
+                    self.Value = sum(self.subpacketValues)
+                case(1):
+                    self.Value = prod(self.subpacketValues)
+                case(2):
+                    self.Value = min(self.subpacketValues)
+                case(3):
+                    self.Value = max(self.subpacketValues)
+                case(5):
+                    self.Value = (self.subpacketValues[0] > self.subpacketValues[1])
+                case(6):
+                    self.Value = (self.subpacketValues[0] < self.subpacketValues[1])
+                case(7):
+                    self.Value = (self.subpacketValues[0] == self.subpacketValues[1])
+                    
+            a = 0
+
                 
 
 test = '110100101111111000101000'
@@ -65,4 +98,7 @@ test2 = '00111000000000000110111101000101001010010001001000000000'
 test_packet = Packet(test)
 test_packet2 = Packet(test2)
 
-print(test_packet.Value)
+Realdeal = Packet(bit_packets)
+
+print(Realdeal.sumVersion)
+print(Realdeal.Value)
